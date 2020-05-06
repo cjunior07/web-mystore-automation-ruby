b48c3dc64e1174a8700567dcb3342e120f062ea0
C1314424 Claudio Da Luz Andrade Junior <cjunior@stefanini.com>
2020-05-06 17:34:40 -0300

    Hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,19 @@
+Before do |scenario|
+  puts "Cenário em execução: " + scenario.name
+end
+
 After do |scenario|
-    # tira um print por favor
-    time = Time.new
-    #Dir.mkdir('shots') unless Dir.exist?('shots')
-    sufix = ('fail' if scenario.failed?) || 'sucess'
-    name = scenario.name.tr(' ','_').downcase
-    shot = "logs/shots/#{sufix}_#{name}_#{time.day}-#{time.month}-#{time.year}.png"
-    page.save_screenshot(shot)
-    embed(shot, 'image/png', 'Clique aqui para ver o print da tela')
+  sufix = ("O cenário FALHOU!" if scenario.failed?) || "Cenário executado com Sucesso !\n\n"
+  puts "#{sufix}"
+  Capybara.reset!
+end
+
+After do |scenario|
+  #tira uma print
+  file_name = scenario.name.tr(" ", "_").upcase
+  sufix = ("FALHOU" if scenario.failed?) || "PASSOU"
+  shot = "logs/shots/#{file_name}_#{sufix}.png"
+  puts "Tirando print da tela"
+  page.save_screenshot(shot)
+  embed(shot, "image/png", "Clique aqui para ver o print da tela")
 end
